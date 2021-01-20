9631c3e0c5763fd2c369a395560788df6a2bbcd9
Eugene Cheah <eugeneqin@gmail.com>
2021-01-20 09:59:37 +0000

    Adding pkg-cache alpine awareness

--- install-pkg-cache.sh
+++ install-pkg-cache.sh
@@ -4,4 +4,10 @@
 mkdir -p ~/.pkg-cache
 
 # Copy the file over - skip if it already exists
-cp -r -n ./pkg-cache/* ~/.pkg-cache
+if [[ `which apk` ]]; then
+    # Alpine does not support the -n optimization
+    # which skips existing files
+    cp -r ./pkg-cache/* ~/.pkg-cache
+else
+    cp -r -n ./pkg-cache/* ~/.pkg-cache
+fi
